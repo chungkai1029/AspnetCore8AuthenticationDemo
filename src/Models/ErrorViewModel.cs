3491c4cad848ea32ea2e4de75f8f5871cecbf007
chungkai1029 <chungkai1029@gmail.com>
2024-08-02 11:30:21 +0800

    Initialize Project

---
+++ src/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AspnetCore8AuthenticationDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
